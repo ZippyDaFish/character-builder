3952d8a2b48f9d31c006464f7da836348f578c6e
Andrew Riker <AMRiker@student.fullsail.edu>
2025-08-09 12:15:34 -0400

    Add styling for navigationHeader

--- app/components/navigationHeader.js
+++ app/components/navigationHeader.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import SignOutButton from "./signOutButton";
+import styles from "../styles/navigationHeader.css";
 
 export default function NavigationHeader() {
     return (
-        <div>
+        <div className="container">
             <Link href="/character-selection">Characters</Link>
             <Link href="/about">About</Link>
             <SignOutButton/>
