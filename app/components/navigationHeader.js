921cc967a10a6694f77c0659ebbc9276000abdf4
ZippyDaFish <andrew.riker21@gmail.com>
2025-08-11 13:27:39 -0400

    Display different navigationHeader dependent upon restrictions of page

--- app/components/navigationHeader.js
+++ app/components/navigationHeader.js
@@ -1,14 +1,25 @@
+'use client';
 import React from "react";
 import Link from "next/link";
 import SignOutButton from "./signOutButton";
+import { usePathname } from "next/navigation";
 import styles from "../styles/navigationHeader.css";
 
 export default function NavigationHeader() {
+    const pathName = usePathname();
+    const isRestricted = pathName.startsWith("/sign");
+
     return (
-        <div className="container">
-            <Link href="/character-selection">Characters</Link>
-            <Link href="/about">About</Link>
-            <SignOutButton/>
+        <div className="page-container">
+            {isRestricted ? (
+                <div></div>
+            ) : (
+                <div className="container">
+                    <Link href="/character-selection">Characters</Link>
+                    <Link href="/about">About</Link>
+                    <SignOutButton/>
+                </div>
+            )}
         </div>
     )
 }
