8343c1a789139550a6d74fc15ffbb5900da5be56
Andrew Riker <AMRiker@student.fullsail.edu>
2025-08-12 12:52:48 -0400

    user auth changes

--- app/components/navigationHeader.js
+++ app/components/navigationHeader.js
@@ -2,12 +2,34 @@
 import React from "react";
 import Link from "next/link";
 import SignOutButton from "./signOutButton";
-import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import styles from "../styles/navigationHeader.css";
 
 export default function NavigationHeader() {
     const pathName = usePathname();
     const isRestricted = pathName.startsWith("/sign");
+    const router = useRouter();
+    const auth = getAuth();
+
+    const [user, setUser] = useState(undefined);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+        setUser(firebaseUser);
+        });
+        return () => unsubscribe();
+    }, [auth]);
+
+    useEffect(() => {
+        if (!user) {
+        router.push("/sign-in");
+        }
+    }, [user, router]);
+
+    if(user === undefined) { return null; }
+    if(user) { console.log("Email: ", user.email); }
 
     return (
         <div className="page-container">
