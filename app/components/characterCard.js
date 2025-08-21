f2f2a553e3779e670e9cea23ab7450c29cdfb01a
Andrew Riker <AMRiker@student.fullsail.edu>
2025-08-21 12:41:30 -0400

    style character card

--- app/components/characterCard.js
+++ app/components/characterCard.js
@@ -1,29 +1,36 @@
+"use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import styles from "../styles/characterCard.css";
+import "../styles/characterCard.css";
 
 export default function CharacterCard({ character, handleDeleteCharacter }) {
-    const router = useRouter();
+  const router = useRouter();
 
-    const handleClickCharacter = () => {
-        router.push(`/character-sheet/${character.id}`);
-    };
-    const handleClickEditCharacter = () => {
-        router.push(`/character-editor/${character.id}`)
-    };
-    const handleClickDelete = () => {
-        handleDeleteCharacter(character.id);
-    }
+  const handleClickCharacter = () => {
+    router.push(`/character-sheet/${character.id}`);
+  };
+  const handleClickEditCharacter = () => {
+    router.push(`/character-editor/${character.id}`);
+  };
+  const handleClickDelete = () => {
+    handleDeleteCharacter(character.id);
+  };
 
-    return (
-        <div className="content-container">
-             <h1>{character.name}</h1>
-            <h3>{character.archetype}</h3>
-            <div className="buttons-container">
-                <button onClick={handleClickCharacter}>View</button>
-                <button onClick={handleClickEditCharacter}>Edit</button>
-                <button onClick={handleClickDelete}>Delete</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="card">
+      <h1 className="name">{character.name}</h1>
+      <h3 className="archetype">{character.archetype}</h3>
+      <div className="buttons">
+        <button className="button view" onClick={handleClickCharacter}>
+          View
+        </button>
+        <button className="button edit" onClick={handleClickEditCharacter}>
+          Edit
+        </button>
+        <button className="button delete" onClick={handleClickDelete}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
--- app/components/characterList.js
+++ app/components/characterList.js
@@ -1,16 +1,17 @@
 'use client';
 import CharacterCard from "./characterCard";
+import "../styles/characterList.css";
 
 export default function CharacterList({ characters, handleDeleteCharacter }) {
-    return (
-        <div>
-            {characters.map((character) => (
-                <CharacterCard
-                    key={character.id}
-                    character={character}
-                    handleDeleteCharacter={handleDeleteCharacter}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="character-list">
+      {characters.map((character) => (
+        <CharacterCard
+          key={character.id}
+          character={character}
+          handleDeleteCharacter={handleDeleteCharacter}
+        />
+      ))}
+    </div>
+  );
 }
